72f5427d6c84e9d4235bff9a83f15f4cdb19e3f5
jeffkrantz <jjkrantz@gmail.com>
2017-08-01 11:36:26 -0500

    Web View Code

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    var webView: UIWebView?
+    let targetURL: String = "https://yahoo.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let width: CGFloat! = self.view.bounds.width
+        let height: CGFloat! = self.view.bounds.height
+        let statusBarHeight: CGFloat! = UIApplication.shared.statusBarFrame.height
+        
+        self.webView = self.createWebView(frame: CGRect(x: 0, y: statusBarHeight, width: width, height:height))
+        
+        self.view.addSubview(self.webView!)
+        
+        let url = NSURL(string: targetURL)
+        let request = NSURLRequest(url: url! as URL)
+        
+        self.webView?.loadRequest(request as URLRequest)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func createWebView(frame: CGRect) -> UIWebView {
+        let _webView = UIWebView()
+        
+        _webView.delegate = self;
+        
+        _webView.frame = frame
+        
+        return _webView
+    }
+    
+    
 
 
 }
