0f7aab29de41c26bc32bf08f3b3f95810052c100
jeffkrantz <jjkrantz@gmail.com>
2017-08-01 11:20:56 -0500

    Initial Commit

---
+++ WebViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewExample
+//
+//  Created by Jeff Krantz on 8/1/17.
+//  Copyright © 2017 Jeff Krantz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
